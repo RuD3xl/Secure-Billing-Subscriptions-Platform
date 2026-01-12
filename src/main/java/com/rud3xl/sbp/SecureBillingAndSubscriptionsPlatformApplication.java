1e63c3f9fb51300fcfbf5610857a81b90e65dcda
RuD3xl <143216073+RuD3xl@users.noreply.github.com>
2026-01-12 15:07:27 +0200

    Initial commit

---
+++ src/main/java/com/rud3xl/sbp/SecureBillingAndSubscriptionsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.rud3xl.sbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBillingAndSubscriptionsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureBillingAndSubscriptionsPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rud3xl/sbp/SecureBillingAndSubscriptionsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rud3xl.sbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBillingAndSubscriptionsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
